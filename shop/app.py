4fd3e5c2a754d63a974c0f63de7f88b0c33d332f
tema40in <tema40in1@mail.ru>
2025-03-30 15:44:26 +0300

    Update app.py

--- shop/app.py
+++ shop/app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
 
+app = Flask("Магазин сюрпризов",
+            template_folder="",
+            static_folder="")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run()
