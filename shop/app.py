b3c04e9e82888ca47d6b38d71c5187dce6c35394
tema40in <tema40in1@mail.ru>
2025-04-13 15:57:42 +0300

    Update app.py

--- shop/app.py
+++ shop/app.py
@@ -1,11 +1,37 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("Магазин сюрпризов",
             template_folder="",
             static_folder="")
 
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///items.db"
+database = SQLAlchemy(app)
+
+class Item(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    image = database.Column(database.String, nullable=False)
+    name = database.Column(database.String, nullable=False)
+    price = database.Column(database.String, nullable=False)
+
+items = []
+item1 = Item(image = 'https://steamuserimages-a.akamaihd.net/ugc/1843658378002555999/D2CB1C4A0B5A01521A8B19C8939A2694D7E3F105/?imw=512&amp;imh=341&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true',
+name = "Icubovich",
+price = "Сектор призп")
+
+items.append(item1)
+
+with app.app_context():
+    database.create_all()
+    existing_items = Item.query.all()
+    if not(existing_items):
+        for item in items:
+            database.session.add(item)
+            database.session.commit()
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    items = Item.query.all()
+    return render_template("index.html", items = items)
 
 app.run()
