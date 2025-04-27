a1e2ff6d8b06e917577d0b5e6833c180ab8bb8be
tema40in <tema40in1@mail.ru>
2025-04-27 15:56:50 +0300

    Update app.py

--- shop/app.py
+++ shop/app.py
@@ -17,9 +17,13 @@ class Item(database.Model):
 items = []
 item1 = Item(image = 'https://steamuserimages-a.akamaihd.net/ugc/1843658378002555999/D2CB1C4A0B5A01521A8B19C8939A2694D7E3F105/?imw=512&amp;imh=341&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true',
 name = "Icubovich",
-price = "Сектор призп")
+price = "211212121")
+item2 = Item(image = "https://aif-s3.aif.ru/images/011/924/89c157b7714dc366ad1bcaf0925ffdd6.jpg",
+             name = "ivan",
+             price = "23000000")
 
 items.append(item1)
+items.append(item2)
 
 chosen = []
 
@@ -37,13 +41,22 @@ with app.app_context():
 def index():
     if request.method == 'POST':
         chosen_name = request.form.get('chosen_name')
-        chosen.append(chosen_name)
+        chosen_price = request.form.get('chosen_price')
+        chosen_image = request.form.get('chosen_image')
+        chosen.append([chosen_name, chosen_price, chosen_image])
     items = Item.query.all()
     return render_template("index.html", items = items)
 
-@app.route("/cart")
+@app.route("/cart", methods=["GET","POST"])
 def cart():
     cart_list = chosen
-    return render_template("/cart.html", cart_list = cart_list)
+    prices = []
+    if request.method == 'POST':
+        cart_list.clear()
+    for i in cart_list:
+        prices.append(int(i[1]))
+    total = sum(prices)
+    return render_template("/cart.html", cart_list = cart_list, total = total)
 
 app.run()
+
